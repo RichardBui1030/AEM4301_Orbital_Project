6f8fe167f303df291c68565dd8ee5951e676f531
RichardBui1030 <116927755+RichardBui1030@users.noreply.github.com>
2024-12-10 09:50:26 -0600

    richard update

--- Final_Design.m
+++ Final_Design.m
@@ -1,9 +1,15 @@
-mu = 1.327E11;
-r1 = 1.496E8;
-r2 = 7.786E8;
+muS = 1.327E11;
+muE = 398600;
+muJ = 126686534;
+rE = 1.496E8;
+rJ = 7.786E8;
 n = sqrt(mu/r2^3);
-t12 = pi * sqrt( (r1 + r2)^3 / (8 * mu));
+t12 = pi * sqrt( (rE + rJ)^3 / (8 * mu));
 
-phi = pi - n*t12
+phi = pi - n*t12;
 
-dv = sqrt((2*mu/r1)-(2*mu/(r1+r2))) - sqrt(mu/r1)
+dv = sqrt((2*muS/r1)-(2*muS/(r1+r2))) - sqrt(muS/r1);
+
+
+
+
